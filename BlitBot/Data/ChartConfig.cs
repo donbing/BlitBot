f033f8682f19cd7a30a9ba82b96df7b177facc10
Chris bingham <christopher.bingham@marken.com>
2022-02-19 19:59:04 +0000

    INPC source generator

--- BlitBot/Data/ChartConfig.cs
+++ BlitBot/Data/ChartConfig.cs
@@ -1,12 +1,34 @@
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using BlitBot.Annotations;
+using DevExpress.Mvvm.CodeGenerators;
+
 namespace BlitBot.Data;
 
-public class ChartConfig
+[GenerateViewModel]
+public partial class ChartConfig : INotifyPropertyChanged
 {
-    public string Symbol { get; set; } = "XBTUSD";
+    [GenerateProperty]
+    public string symbol  = "XBTUSD";
+    [GenerateProperty]
+    public string locale = "en";
+    [GenerateProperty]
+    public string timeZone = "Etc/UTC";
+    [GenerateProperty]
+    public string interval = "240";
 
     public event EventHandler OnChartChanged = 
         (sender, args) => { };
 
     public void Updated() => 
         OnChartChanged.Invoke(this, new EventArgs());
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    [NotifyPropertyChangedInvocator]
+    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        Updated();
+    }
 }
--- BlitBot/Data/ChartService.cs
+++ BlitBot/Data/ChartService.cs
@@ -1,8 +1,9 @@
+using System.Text.Json;
 using Microsoft.JSInterop;
 
 namespace BlitBot.Data;
 
-public class ChartService
+public class ChartService 
 {
     readonly IJSRuntime js;
     public readonly ChartConfig config;
@@ -14,5 +15,19 @@ public class ChartService
     }
 
     public async Task DrawChart() => 
-        await js.InvokeVoidAsync("charting.show", config.Symbol);
+        await js.InvokeVoidAsync("charting.show", config.Symbol, config.Locale, config.TimeZone, config.Interval);
+
+    string targetUrl = "https://s3.tradingview.com/external-embedding/embed-widget-technical-analysis.js";
+    public async Task LoadTechnicalAnalysis(string containerElement) =>
+        await js.InvokeVoidAsync("scriptLoader", targetUrl, containerElement, JsonSerializer.Serialize(new
+        {
+            interval = "1m",
+            width = "100%",
+            isTransparent = false,
+            height = "100%",
+            symbol = config.Symbol,
+            showIntervalTabs = true,
+            locale = config.Locale,
+            colorTheme = "dark",
+        }));
 }
