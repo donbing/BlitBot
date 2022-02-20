222e7171bc3468795f35122e1de23782ce13b26f
donbing <ccbing@gmail.com>
2022-02-20 19:09:33 +0000

    give test a proper name

---
+++ Tests/CanSearchTimeZones.cs
@@ -0,0 +1,21 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using BlitBot.Data;
+using NUnit.Framework;
+
+namespace Tests;
+
+public class CanSearchTimeZones
+{
+    readonly ChartService svc = new(null, new ChartConfig());
+    IEnumerable<string>? tzs;
+
+    [OneTimeSetUp] public async Task Setup() => 
+        tzs = await svc.SearchTimeZones("AMeriCa");
+
+    [Test] public void GetTimeZones() => 
+        Assert.That(tzs, Is.Not.Empty);
+
+    [Test] public void AreOrdered() => 
+        Assert.That(tzs, Is.Ordered);
+}
