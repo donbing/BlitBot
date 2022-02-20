41a6190f5e283ea910014b0fcbe3c1cb5f35911a
donbing <ccbing@gmail.com>
2022-02-20 21:45:03 +0000

    option to sleep/wake the screen

---
+++ BlitBot/Data/ShellHelper.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics;
+
+namespace BlitBot.Data;
+
+public static class ShellHelper
+{
+    public static void LinuxSleepScreen(bool state) 
+    {
+        var onOrOff = state ? "on" : "off";
+
+        var cmd = $"DISPLAY=:0.0 xset dpms force {onOrOff}";
+
+        cmd.Bash();
+    }
+
+    public static string Bash(this string cmd)
+    {
+        var escapedArgs = cmd.Replace("\"", "\\\"");
+
+        var process = new Process()
+        {
+            StartInfo = new ProcessStartInfo
+            {
+                FileName = "/bin/bash",
+                Arguments = $"-c \"{escapedArgs}\"",
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+                CreateNoWindow = true,
+            }
+        };
+
+        process.Start();
+        string result = process.StandardOutput.ReadToEnd();
+        process.WaitForExit();
+
+        return result;
+    }
+}
