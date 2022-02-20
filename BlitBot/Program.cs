e0ccac835340dffeb191673b32286aa4c0d0850c
donbing <ccbing@gmail.com>
2022-02-20 19:09:13 +0000

    extract app builder extensions

--- BlitBot/Program.cs
+++ BlitBot/Program.cs
@@ -1,26 +1,12 @@
-using BlitBot.Data;
-
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddRazorPages();
-builder.Services.AddServerSideBlazor();
-builder.Services.AddTradingView();
-builder.Services.AddHttpClient();
+builder.Services.AddServices();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-}
-
+app.UseErrorHandlerForLiveEnvs();
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
-
 app.Run();
---
+++ BlitBot/WebAppExtensions.cs
@@ -0,0 +1,20 @@
+﻿using BlitBot.Data;
+
+public static class WebAppExtensions 
+{
+    public static void UseErrorHandlerForLiveEnvs(this WebApplication app)
+    {
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Error");
+        }
+    }
+
+    public static void AddServices(this IServiceCollection services)
+    {
+        services.AddRazorPages();
+        services.AddServerSideBlazor();
+        services.AddTradingView();
+        services.AddHttpClient();
+    }
+}
